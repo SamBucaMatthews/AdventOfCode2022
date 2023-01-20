b54e56786cead8f139e5974944e71779a51b4341
Sam Matthews <36134497+SamBucaMatthews@users.noreply.github.com>
2023-01-20 11:53:32 +0000

    Add Point with tests

---
+++ AdventOfCode2022/AdventOfCode2022.Solutions/DayFourteen/Cave.cs
@@ -0,0 +1,20 @@
+﻿namespace AdventOfCode2022.Solutions.DayFourteen;
+
+public class Cave
+{
+}
+
+public record Point(int Row, int Column)
+{
+    public static Point Parse(string input)
+    {
+        var parts = input.Split(",");
+
+        if (parts.Length != 2 || !int.TryParse(parts[0], out var column) || !int.TryParse(parts[1], out var row))
+        {
+            throw new ArgumentException(input);
+        }
+        
+        return new Point(row, column);
+    }
+}
